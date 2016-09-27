c0d0f4473dbc78edddff42448c151ab3e2127e90
Nicolas Mervaillie <nmervaillie@salto-consulting.com>
2016-09-27 07:02:58 +0200

    Be able to run jqa server as a background process #4

--- src/main/java/com/buschmais/jqassistant/commandline/task/ServerTask.java
+++ src/main/java/com/buschmais/jqassistant/commandline/task/ServerTask.java
@@ -1,19 +1,20 @@
 package com.buschmais.jqassistant.commandline.task;
 
+import java.io.IOException;
+import java.util.List;
+
 import com.buschmais.jqassistant.commandline.CliExecutionException;
 import com.buschmais.jqassistant.core.plugin.api.PluginRepositoryException;
 import com.buschmais.jqassistant.core.store.api.Store;
 import com.buschmais.jqassistant.core.store.impl.EmbeddedGraphStore;
 import com.buschmais.jqassistant.scm.neo4jserver.api.Server;
 import com.buschmais.jqassistant.scm.neo4jserver.impl.ExtendedCommunityNeoServer;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.util.List;
 
 /**
  * @author jn4, Kontext E GmbH, 23.01.14
@@ -23,9 +24,11 @@ public class ServerTask extends AbstractTask {
 
     public static final String CMDLINE_OPTION_SERVERADDRESS = "serverAddress";
     public static final String CMDLINE_OPTION_SERVERPORT = "serverPort";
+    public static final String CMDLINE_OPTION_DAEMON = "daemon";
 
     private String serverAddress;
     private int serverPort;
+    private Boolean runAsDaemon;
 
     @Override
     protected void executeTask(final Store store) throws CliExecutionException {
@@ -37,14 +40,20 @@ public class ServerTask extends AbstractTask {
             throw new CliExecutionException("Cannot get plugins.", e);
         }
         server.start();
-        LOGGER.info("Running server");
-        LOGGER.info("Press <Enter> to finish.");
-        try {
-            System.in.read();
-        } catch (IOException e) {
-            throw new CliExecutionException("Cannot read from console.", e);
-        } finally {
-            server.stop();
+
+        if (runAsDaemon) {
+            // let the neo4j daemon do the job
+            LOGGER.info("Running server. Use <Ctrl-c> to stop server.");
+        } else {
+            LOGGER.info("Running server");
+            LOGGER.info("Press <Enter> to finish.");
+            try {
+                System.in.read();
+            } catch (IOException e) {
+                throw new CliExecutionException("Cannot read from console.", e);
+            } finally {
+                server.stop();
+            }
         }
     }
 
@@ -55,11 +64,14 @@ public class ServerTask extends AbstractTask {
                 .create(CMDLINE_OPTION_SERVERADDRESS));
         options.add(OptionBuilder.withArgName(CMDLINE_OPTION_SERVERPORT).withDescription("The binding port of the server.").hasArgs()
                 .create(CMDLINE_OPTION_SERVERPORT));
+        options.add(OptionBuilder.withArgName(CMDLINE_OPTION_DAEMON).withDescription("Do not wait for <Enter> on standard input to stop the server.")
+                .create(CMDLINE_OPTION_DAEMON));
     }
 
     @Override
     public void withOptions(CommandLine options) {
         serverAddress = getOptionValue(options, CMDLINE_OPTION_SERVERADDRESS, ExtendedCommunityNeoServer.DEFAULT_ADDRESS);
         serverPort = Integer.valueOf(getOptionValue(options, CMDLINE_OPTION_SERVERPORT, Integer.toString(ExtendedCommunityNeoServer.DEFAULT_PORT)));
+        runAsDaemon = options.hasOption(CMDLINE_OPTION_DAEMON);
     }
 }
